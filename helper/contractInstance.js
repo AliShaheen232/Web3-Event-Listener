6d1ae0cc6d213619d359c4b8cd19feb7e9a15ec7
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-02-07 21:42:56 +0500

    Update contractInstance.js

--- helper/contractInstance.js
+++ helper/contractInstance.js
@@ -3,10 +3,16 @@ const CONTRACT_ABI = require("../abi/zentuSimpleStaking_ABI.json");
 
 let web3;
 let contract;
-
 module.exports = createInstance = async (address) => {
   web3 = await injectedWeb3.getSocket();
   contract = new web3.eth.Contract(CONTRACT_ABI, address);
 
   return { web3, contract };
 };
+
+// module.exports = createInstance = async (address) => {
+//   web3 = await injectedWeb3.getSocket();
+//   contract = new web3.eth.Contract(CONTRACT_ABI, address);
+
+//   return { web3, contract };
+// };
